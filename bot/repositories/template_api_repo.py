7ee70f1f70d0d1127b0e4b26c196ba7ccb1479b9
Thomas Van Iseghem <thomas.van.iseghem@student.howest.be>
2022-02-09 23:55:32 +0100

    Added API repo error handling

--- bot/repositories/template_api_repo.py
+++ bot/repositories/template_api_repo.py
@@ -1,14 +1,18 @@
 from pipes import Template
 
-
 import requests
 import os
 import logging
 
 class TemplateApiRepository:
-    API_URL = os.getenv('API_URL', 'http://crash:8080')
+    API_URL = os.getenv('API_URL', 'http://fastapi:8080')
     logging.info(f'API_URL: {API_URL}')
 
     async def get_example_data():
-        response = requests.get(TemplateApiRepository.API_URL + '/example')
-        return response.json()
+        try:
+            response = requests.get(TemplateApiRepository.API_URL + '/example')
+            return response.json()
+
+        except Exception as e:
+            logging.error(e)
+            return {"data": "Error, API service is not available"}
